24a9300ffd110f0c9cfc84b530833eb4dde82d7b
Dhruv Jain <djain@cs.washington.edu>
2018-11-25 22:54:40 -0800

    frontend- waveform view v1 done

--- VolumeMeter.py
+++ VolumeMeter.py
@@ -2,8 +2,8 @@ import pyaudio
 import numpy as np
 import sys
 
-CHUNK = 2**11
 RATE = 44100
+CHUNK = int(RATE/1) 	# 1 update per second
 
 p=pyaudio.PyAudio()
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
@@ -13,9 +13,11 @@ i=0
 while True:
     data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
     peak=np.average(np.abs(data))*2
-    bars="#"*int(50*peak/2**16)
-    print("%04d %05d %s"%(i,peak,bars))
-    i+=1
+    bars=50*peak/2**16
+    print("%s"%(bars))
+    #bars="#"*int(50*peak/2**16)
+    #print("%04d %05d %s"%(i,peak,bars))
+    #i+=1
     sys.stdout.flush()
 
 stream.stop_stream()
